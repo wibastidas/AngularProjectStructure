5087eaa067f0544a2660988bbddc60f5aaae7e5d
wibastidas <wibastidas77@gmail.com>
2020-05-03 11:42:43 -0300

    Create layout component with header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LayoutComponent,
+    children: [
+      {
+        path:'',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home', 
+        component: HomeComponent
+      },
+      {
+        path: 'workspace', 
+        component: WorkspaceComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    WorkspaceComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
